6bf080769aff5a30aa8db5c507a21a05435468a5
Darkx23312 <130650239+Darkx-dev@users.noreply.github.com>
2023-12-03 03:16:27 +0530

    main

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
-import Image from "next/image";
+import { Image } from "@nextui-org/react";
 import { Instagram } from "lucide-react";
 import React from "react";
 
