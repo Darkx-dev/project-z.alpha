ed39f19b7c18ff03fbdc1dacd58d2efa665ba9fd
Darkx23312 <130650239+Darkx-dev@users.noreply.github.com>
2023-12-25 16:46:53 +0530

    main

--- src/components/ShowCase.tsx
+++ src/components/ShowCase.tsx
@@ -8,10 +8,23 @@ import { useGSAP } from "@gsap/react";
 export default function ShowCase() {
     const projects = projectsApi
   useGSAP(() => {
-    
+    const tl = gsap.timeline({})
+    tl.from('#project-card', {
+        scale: 0,
+        yPercent: -200,
+        duration: .5,
+        opacity: 0,
+        ease: 'ease',
+        stagger: {
+            amount: .5,
+            grid: 'auto',
+            from: 'start',
+            ease: 'ease'
+        }
+    })
   });
   return (
-    <div className="flex gap-3">
+    <div className="flex flex-wrap gap-3">
       {projects.map((project) => (
         <Card key={project.id} title={project.name} imgUrl={project.imgUrl} url={project.liveUrl}/>
       ))}
